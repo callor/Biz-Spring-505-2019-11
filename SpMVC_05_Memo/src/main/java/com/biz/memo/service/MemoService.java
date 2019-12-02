8da50ae41b37623706c17434eb2e61ee4d96319b
callor <callor@callor.com>
2019-12-02 16:49:34 +0900

    메모장

---
+++ SpMVC_05_Memo/src/main/java/com/biz/memo/service/MemoService.java
@@ -0,0 +1,48 @@
+package com.biz.memo.service;
+
+import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.biz.memo.domain.MemoDTO;
+import com.biz.memo.persistence.MemoDao;
+
+// @Repository를 사용할수도 있다.
+@Service
+public class MemoService {
+	
+	@Autowired
+	SqlSession sqlSession;
+	
+	MemoDao mDao;
+	
+	/*
+	 * Service를 사용하려고 시도하면
+	 * sqlSession으로 부터 MemoDao mapper를 추출하여
+	 * mDao 객체를 사용할수 있도록 초기화 하라
+	 */
+	@Autowired
+	public void getMapper() {
+		mDao = sqlSession.getMapper(MemoDao.class);
+	}
+
+	public List<MemoDTO> getAllList() {
+		// TODO 전체 메모리스트를 DB로 부터 가져와서 Controller로 리턴
+		return mDao.selectAll();
+	}
+
+	public List<MemoDTO> getSearchList(String m_subject) {
+		// TODO 제목으로 검색하기
+		MemoDTO memoDTO = MemoDTO.builder()
+							.m_subject(m_subject)
+							.build();
+		return mDao.findBySearch(memoDTO);
+	}
+
+	public int insert(MemoDTO memoDTO) {
+		return mDao.insert(memoDTO);
+	}
+
+}
