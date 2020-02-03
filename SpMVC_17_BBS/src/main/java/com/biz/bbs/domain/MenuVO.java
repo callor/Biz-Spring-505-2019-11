fd3435c4dfc484c6919e01c1bb95dc2bce97cbea
callor <callor@callor.com>
2020-02-03 16:11:21 +0900

    bbs

---
+++ SpMVC_17_BBS/src/main/java/com/biz/bbs/domain/MenuVO.java
@@ -0,0 +1,32 @@
+package com.biz.bbs.domain;
+
+import java.util.List;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+public class MenuVO {
+
+	private String menu_id;
+	private String menu_p_id;
+	private String menu_title;
+	private String menu_href;
+	
+	/*
+	 * drop down을 구현하기 위해서 
+	 * 자기 자신을 List로 설정
+	 */
+	List<MenuVO> drop_menus;
+	
+}
+
+
+
