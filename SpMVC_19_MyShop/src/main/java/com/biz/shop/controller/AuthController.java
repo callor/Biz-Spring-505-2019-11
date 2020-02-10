5bb2935d41ba6238ce77d536f4484f8d3046d3d7
callor <callor@callor.com>
2020-02-10 17:34:03 +0900

    shop

---
+++ SpMVC_19_MyShop/src/main/java/com/biz/shop/controller/AuthController.java
@@ -0,0 +1,23 @@
+package com.biz.shop.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class AuthController {
+	
+	@RequestMapping(value="login",method=RequestMethod.GET)
+	public String login() {
+		return "login";
+	}
+
+	@RequestMapping(value="/auth/test",method=RequestMethod.GET)
+	public String test() {
+		return "auth/test";
+	}
+
+	
+	
+}
